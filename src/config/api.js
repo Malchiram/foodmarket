c52766e027c7335f0356c3cbdb57710c78bbf5d3
Malchiram <storeflash04@gmail.com>
2025-01-15 13:20:42 +0700

    changebackend

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create base URL API
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.REACT_APP_MIDTRANS_CLIENT_KEY,
 });
 
 export const APILOC = axios.create({
